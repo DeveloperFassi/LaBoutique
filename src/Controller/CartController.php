dc18309bd12ffb757eb9a4788192c33aa41d4b19
Devfassi <daka.bambara@hotmail.fr>
2020-09-16 17:45:37 +0200

    - Logic of Cart is done, still need to implement the front

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -1,26 +1,65 @@
 <?php
 
-
 namespace App\Controller;
 
-
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Config\Definition\Exception\Exception;
+use Symfony\Component\HttpClient\Internal\ClientState;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * Class GuidesController
- * @package App\Controller
- * @Route ("/cart")
+ * @Route("/cart")
  */
 class CartController extends AbstractController
 {
     /**
-     * @return Response
-     * @Route ("/", name="cart_index")
+     * @param ProductRepository $productRepository
+     * @param SessionInterface $session
+     * @Route("/", name="cart_index")
      */
-    public function index(): Response
+    public function index(ProductRepository $productRepository, SessionInterface $session)
     {
-       return $this->render('public/cart.html.twig');
+        $cart = $session->get('cart');
+
+        $cartToDisplay = [];
+        $total = 0;
+        foreach ($cart as $id => $quantity) {
+            $product = $productRepository->find($id);
+            $total += $product->getPrice() * $quantity;
+            $cartToDisplay[] = [
+                'product' => $product,
+                'quantity' => $quantity,
+            ];
+        }
+
+
+        return $this->render('cart/index.html.twig', [
+            'cartToDisplay' => $cartToDisplay,
+            'total' => $total
+        ]);
+    }
+
+
+    /**
+     * @param $id
+     * @param SessionInterface $session
+     * @Route("/add/{id}", name = "cart_add", methods = {"POST", "GET"})
+     */
+    public function add($id, SessionInterface $session)
+    {
+        $cart = $session->get('cart', []);
+
+        if ($id != 0 && !empty($cart[$id])) {
+            $cart[$id]++;
+        } else if ($id != 0) {
+            $cart[$id] = 1;
+        } else {
+            throw new Exception('Id can\'t be 0');
+        }
+        $session->set('cart', $cart);
     }
 }
